c09c1895661527bb63631536650d487426f4cfdd
A. <65830011+uflak09842@users.noreply.github.com>
2025-03-02 23:45:48 +0300

    backend geçiom bb

--- app/(screens)/detail/DetailScreen.jsx
+++ app/(screens)/detail/DetailScreen.jsx
@@ -1,9 +1,19 @@
+import React, { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
-import React from 'react'
 import { useLocalSearchParams } from 'expo-router'
 
 const DetailScreen = () => {
   const mv = useLocalSearchParams();
+
+  const [ movie, setMovie ] = useState();
+
+  useEffect(() => {
+    const getMovie = async () => {
+      const movie = await axios.get(`${process.env.EXPO_PUBLIC_SERVER_URL}/getMovie`);
+
+      
+    }
+  }, []);
 
   return (
     <View>
